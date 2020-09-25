68a86ea965f40f5004a8493c6e34fea1e3c40757
nihadatakishiyev <niko.atakishiyev@gmail.com>
2020-09-25 16:16:30 +0400

    fills in lumenapicontroller

--- backend/app/Http/Controllers/LumenApiController.php
+++ backend/app/Http/Controllers/LumenApiController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class LumenApiController extends Controller
 {
-    //
+    public function requestAsanFinance(Request $request){
+
+        $res = Http::get('http://localhost/lumen/public/api/asanfinance');
+
+        if ($request->expectsJson())
+            return $res;
+        else return view('is\asanfinance')->with('query', $res);
+
+    }
 }
